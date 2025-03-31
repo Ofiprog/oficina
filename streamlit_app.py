b5cc3293850d28c5edf9547dad019f753e51c3e2
Ofiprog <ofi_cba24@icloud.com>
2025-03-31 03:06:11 +0000

    agregar supabase

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from supabase import create_client,Client
+
+url:str="https://yundfqluztuthknvmoco.supabase.co"
+key:str="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1bmRmcWx1enR1dGhrbnZtb2NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzODcxNjAsImV4cCI6MjA1ODk2MzE2MH0.P20V-2gUuVuwmbh59kKzuM4kMjZco0x23ynic8RZhpc"
+supabase: Client=create_client(url,key)
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Ladadadadadadbuilding! For hadasdadelp gggfdbb6543333and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+data=supabase.table("Usuarios").select("*").execute()
+st.write(data)
